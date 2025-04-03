18b009d51647431e5dc50172d3b1f4d0c7d36e3a
Max Biedermann <maxbiedermann@Laptop-von-Max.fritz.box>
2025-04-03 15:12:24 +0200

    neuversuch

---
+++ src/FilmIdeenSlotMachine.jsx
@@ -0,0 +1,3 @@
+export default function FilmIdeenSlotMachine() {
+  return <div style={{ padding: 40 }}><h1>🎬 Film- & Serien-Ideen-Slotmachine</h1><p>Testversion online bereit.</p></div>;
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import FilmIdeenSlotMachine from './FilmIdeenSlotMachine';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FilmIdeenSlotMachine />
+  </React.StrictMode>,
+);
